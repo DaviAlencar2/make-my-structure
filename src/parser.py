6ce58e641c669bc34f4c941e86fc67ba8a035821
Davi Leite Alencar <davilt.alencar@gmail.com>
2025-01-15 16:42:44 -0300

    feat: create parse_structure function

---
+++ src/parser.py
@@ -0,0 +1,38 @@
+def parse_structure(file_path) -> dict:
+
+    structure = {}
+    stack = [structure]
+    previous_indent = 0
+
+    with open(file_path,"r",encoding="utf-8") as file:
+        lines = file.readlines()
+
+    for line in lines:
+        line = line.rstrip()
+        if not line.strip():
+            continue
+
+        indent_level = len(line) - len(line.lstrip("│ "))
+
+        if line.endswith("/"):
+            folder_name = line.replace('├── ', '').replace('└──','').replace('│   ', '').replace('/', '').strip()
+            current_folder = {}
+            stack[-1][folder_name] = current_folder
+            stack.append(current_folder)
+
+        else:
+            file_name = line.replace('├──', '').replace('└──','').replace('│   ','').strip()
+            stack[-1][file_name] = None
+
+            # Ajustar a pilha se o nível de indentação diminuiu
+            if indent_level < previous_indent:
+                stack.pop()
+                
+            previous_indent = indent_level
+
+    return structure
+
+#teste rapido
+if __name__ == "__main__":
+    structure = parse_structure("./examples/sample_structure.txt")
+    print(structure)
